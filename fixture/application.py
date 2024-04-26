6872b049e51acb480bf21b503247344f2895a9a7
solvei9 <solvei9@yandex.ru>
2024-04-26 19:35:03 +0700

    Add test for signing up new account

--- fixture/application.py
+++ fixture/application.py
@@ -1,11 +1,14 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.project import ProjectHelper
+from fixture.james import JamesHelper
+from fixture.signup import SignupHelper
+from fixture.mail import MailHelper
 
 
 class Application:
 
-    def __init__(self, browser, base_url):
+    def __init__(self, browser, config):
         if browser == "firefox":
             self.wd = webdriver.Firefox()
         elif browser == "chrome":
@@ -15,8 +18,12 @@ class Application:
         else:
             raise ValueError("Unrecognized browser %s", browser)
         self.session = SessionHelper(self)
+        self.james = JamesHelper(self)
+        self.signup = SignupHelper(self)
+        self.mail = MailHelper(self)
         self.project = ProjectHelper(self)
-        self.base_url = base_url
+        self.config = config
+        self.base_url = config['web']["baseUrl"]
 
     def is_valid(self):
         try:
