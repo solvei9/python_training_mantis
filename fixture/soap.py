07c9b26fee238ff2c859c998e982d644ca8bd9fc
solvei9 <solvei9@yandex.ru>
2024-05-01 09:27:41 +0700

    Get base url from configuration file

--- fixture/soap.py
+++ fixture/soap.py
@@ -8,7 +8,7 @@ class SoapHelper:
         self.app = app
 
     def can_login(self, username, password):
-        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.app.base_url + "api/soap/mantisconnect.php?wsdl")
         try:
             client.service.mc_login(username, password)
             return True
@@ -16,7 +16,7 @@ class SoapHelper:
             return False
 
     def get_project_list(self, username, password):
-        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.app.base_url + "api/soap/mantisconnect.php?wsdl")
         try:
             return client.service.mc_projects_get_user_accessible(username, password)
         except WebFault:
